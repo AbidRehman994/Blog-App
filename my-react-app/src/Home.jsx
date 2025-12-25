72749e6330c927b3efc964219ec7efd28f84f5d4
Abid Rehman <abidwazir994@gmail.com>
2025-12-25 12:40:09 +0500

    feat: catch error

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -5,6 +5,7 @@ import BlogList from './BlogList';
 const Home=()=>{
   const[blogs,setBlogs]=useState(null);
   const[isPending,setisPending]=useState(true);
+  const [error,setError]=useState(null);
 
   const handleDelete=(id)=>{
     const newBlogs=blogs.filter(blog=>blog.id!== id);
@@ -14,17 +15,26 @@ const Home=()=>{
   useEffect(()=>{
   fetch('http://localhost:8000/blogs')
   .then(res=>{
+    if(!res.ok){
+      throw Error("couldn't fetch data");
+    }
     return res.json()
   })
   .then(data=>{
     console.log(data)
     setBlogs(data)
     setisPending(false);
+    setError(null);
+  })
+  .catch(err=>{
+    setError(err.message);
+    setisPending(false);
   })
   },[]);
 
   return(
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
    {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}   
     {/*filtering*/}
