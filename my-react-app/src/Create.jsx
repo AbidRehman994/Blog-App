d813e43bfd36a2588a5ed64c1587212fd710fe6d
Abid Rehman <abidwazir994@gmail.com>
2025-12-29 15:48:56 +0500

    feat: delete blog button

--- my-react-app/src/Create.jsx
+++ my-react-app/src/Create.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create=()=>{
     const [title,setTitle]=useState('');
     const [body,setBody]=useState('');
     const [author,setAuthor]=useState('');
     const [isPending,setIsPending]=useState(false);
+    const history = useHistory();
 
     const handleSubmit=(e)=>{
            e.preventDefault();
@@ -17,7 +19,9 @@ const Create=()=>{
             body:JSON.stringify(blog)
            }).then(()=>{
             console.log('new blog added');
-            setIsPending(false)
+            setIsPending(false);
+            // history.go(-1);
+            history.push('/');
            })
     }
     return(
