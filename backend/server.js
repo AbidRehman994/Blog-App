52fe76a2dbeaf1d8deb731747d4f0e7a8eb3967d
Abid Rehman <abidwazir994@gmail.com>
2026-07-10 18:11:00 +0500

    feat: add Express request logger

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ const express= require('express');
 //express app
 const app = express();
 
+app.use((req,res,next)=>{
+  console.log(req.path, req.method)
+  next()
+})
+
 //react to reqs : route handler
 app.get('/',(req,res)=>{
   res.send('Hello')
 });
+
 
 //listen for requests
 app.listen(process.env.PORT,()=>{
