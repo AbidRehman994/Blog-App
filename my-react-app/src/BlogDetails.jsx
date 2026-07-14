0bf5027f9038ca81eb6bc0533713bab9a11bb94b
Abid Rehman <abidwazir994@gmail.com>
2026-07-15 01:15:26 +0500

    Replace JSON Server with Express API

--- my-react-app/src/BlogDetails.jsx
+++ my-react-app/src/BlogDetails.jsx
@@ -4,11 +4,11 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails=()=>{
     const {id}=useParams();
-    const {data :blog,error,isPending}=useFetch('http://localhost:8000/blogs/'+id);
+    const {data :blog,error,isPending}=useFetch("http://localhost:5000/api/blogs/" + id);
     const history = useHistory();
 
     const handleClick=()=>{
-      fetch("http://localhost:8000/blogs/"+ blog.id,{
+      fetch("http://localhost:5000/api/blogs/" + blog._id,{
         method: "DELETE"
     }).then(()=>{
          history.push('/');
