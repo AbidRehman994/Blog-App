b96b66bfa78c2609bd7064cf68eee42c16cdf864
Abid Rehman <abidwazir994@gmail.com>
2026-08-20 12:02:40 +0500

    add password hashing and user authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,29 +1,38 @@
 const mongoose = require("mongoose");
+const bcrypt = require ('bcrypt')
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
 
     email: {
       type: String,
       required: true,
       unique: true,
-      trim: true,
+      trim: true
     },
 
     password: {
       type: String,
-      required: true,
+      required: true
     },
   },
   { timestamps: true }
 );
+
+//static signup method 
+userSchema.statics.signup = async function (email,password)  {
+const exists = await this.findOne({email})
+if(exists) {
+  throw Error("email already exists")
+}
+const salt = await bcrypt.genSalt(10)
+const hash = await bcrypt.hash(password,salt)
+
+const user = await this.create({email,password: hash})
+
+return user;
+}
 
 module.exports = mongoose.model("User", userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,4 +14,4 @@ router.post('/login', loginUser)
 
 router.post('/signup', signupUser)
 
-module.exports = router
+module.exports = router;
