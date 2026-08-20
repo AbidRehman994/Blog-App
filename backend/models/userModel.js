45934b93635141c89dd1027c18bc7abe460e5d17
Abid Rehman <abidwazir994@gmail.com>
2026-08-20 12:35:35 +0500

    add password and email validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require ('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema;
 
@@ -23,10 +24,24 @@ const userSchema = new Schema(
 
 //static signup method 
 userSchema.statics.signup = async function (email,password)  {
+
+  //validation
+if(!email || !password) {
+  throw Error('All fields must be filled')
+}
+if(!validator.isEmail(email)){
+  throw Error("email is not valid")
+}
+if(!validator.isStrongPassword(password)){
+  throw Error("password not strong enough")
+}
+
 const exists = await this.findOne({email})
+
 if(exists) {
   throw Error("email already exists")
 }
+
 const salt = await bcrypt.genSalt(10)
 const hash = await bcrypt.hash(password,salt)
 
